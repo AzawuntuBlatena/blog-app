5b1c7c47d9958aaa6de53f173b9957524909053f
AzawuntuBlatena <azabla1227@gmail.com>
2024-08-18 23:41:12 +0300

    connet to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 
-const app = express()
+dotenv.config()
+
+// mongodb connection!
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is Connected!!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDb", err);
+  });
 
 
+const app = express();
+
+
+//runing the server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
